8d1f51d9575c9d3a39a784c0a2303037481612f8
Linval Muchapirei <h170407h@hit.ac.zw>
2022-09-11 16:58:51 +0200

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by linvalM on 11/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
