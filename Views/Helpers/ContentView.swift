d4878e547aba66f39bc8c57ad30eb30525afb7b6
Linval Muchapirei <h170407h@hit.ac.zw>
2022-09-18 10:36:30 +0200

    Add some unversioned files

---
+++ Views/Helpers/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  LandMark
+//
+//  Created by linvalM on 11/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
+    var body: some View {
+        TabView(selection: $selection){
+            CategoryHome()
+                .tabItem{
+                    Label("Featured",systemImage:"star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                    .tabItem{
+                        Label("List",systemImage: "list.bullet")
+                    }
+                .tag(Tab.list)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .environmentObject(ModelData())
+        }
+    }
+}
