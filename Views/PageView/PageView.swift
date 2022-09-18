0ea5dfb0107857bce0a468d86d2375e6d874659e
Linval Muchapirei <h170407h@hit.ac.zw>
2022-09-18 16:49:04 +0200

    Add presentation of the PageViewController in a swiftUI View

---
+++ Views/PageView/PageView.swift
@@ -0,0 +1,22 @@
+//
+//  PageView.swift
+//  LandMark
+//
+//  Created by linvalM on 18/9/2022.
+//
+
+import SwiftUI
+
+struct PageView<Page:View>: View {
+    var pages: [Page]
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map{FeatureCard(landmark: $0)})
+            .aspectRatio(3 / 2,contentMode: .fit)
+    }
+}
