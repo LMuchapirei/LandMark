d71d5467076dba0d10b169044d35c7eee0164464
Linval Muchapirei <h170407h@hit.ac.zw>
2022-09-25 16:16:04 +0200

    Ability to update a value from the view controller and then make it available in the Swift UI View via a binding

--- Views/PageView/PageViewController.swift
+++ Views/PageView/PageViewController.swift
@@ -16,21 +16,24 @@ struct PageViewController<Page:View>: UIViewControllerRepresentable {
     }
     
     var pages: [Page]
+    @Binding var currentPage:Int
     
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
-            transitionStyle: .pageCurl,
+            transitionStyle: .scroll,
             navigationOrientation: .horizontal
         )
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([context.coordinator.controllers[0]], direction: .forward, animated: true)
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject,UIPageViewControllerDataSource {
+    class Coordinator: NSObject,UIPageViewControllerDataSource,UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -64,5 +67,21 @@ struct PageViewController<Page:View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        
+        // Because SwiftUI calls this method whenever a page switching animation completes, you
+        
+        func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool) {
+        if completed,
+           let visibleViewController = pageViewController.viewControllers?.first,
+           let index = controllers.firstIndex(of: visibleViewController) {
+            parent.currentPage = index
+        }
+    
+}
+
     }
 }
